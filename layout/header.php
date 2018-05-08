4aa48fbec40575eb192c61531b005f2472d47e22
Akishino1998 <ekopujianto48@gmail.com>
2018-05-09 00:39:41 +0800

    add file

---
+++ layout/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar-fixed">
+	<div class="nav-wrapper light-blue lighten-1">
+		<a href="#" class="brand-logo dc_head">Doctor Computer</a>
+		<ul class="right">
+			<li><a href="#">MENU1</a></li>
+			<li><a href="#">MENU2</a></li>
+			<li><a href="#">MENU3</a></li>
+		</ul>
+	</div>
+</nav>
